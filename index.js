a83c347a64394d232cb55fbdd41d410c04afa80e
Christopher <rndchris@gmail.com>
2024-06-04 22:04:58 -0500

    add trope api get requests and expanded bingo card generator for all tropes

--- index.js
+++ index.js
@@ -5,13 +5,24 @@ import fs from "fs"
 const app = express();
 const port = 3500;
 global.tropes = []
+global.allTropes = []
 var cardCounter = 0
 
 fs.readFile("./tropes.txt", "utf8", (err, data) => {
     if (err) throw err;
     tropes = data.split('\n');
-    console.log(tropes.length + " tropes loaded");
+    console.log(tropes.length + " tropes loaded to main card generator");
     if (tropes.length<24){
+        throw "Insufficient tropes for bingo card generation";
+    }
+})
+
+//For bingo cards using full trope list instead of just the most common ones
+fs.readFile("./all-tropes.txt", "utf8", (err, data) => {
+    if (err) throw err;
+    allTropes = data.split('\n');
+    console.log(allTropes.length + " tropes loaded to all tropes card generator");
+    if (allTropes.length<24){
         throw "Insufficient tropes for bingo card generation";
     }
 })
@@ -24,6 +35,27 @@ app.get("/", (req,res) => {
     cardCounter++;
     console.log(Date() + req.ip + " Requested a Bingo Card. " + cardCounter + " total cards requested since last restart.");
     res.render("index.ejs", {trope: squares});
+})
+
+app.get("/expanded", (req,res) => {
+    let squares = generateExpandedCard();
+    cardCounter++;
+    console.log(Date() + req.ip + " Requested a Bingo Card. " + cardCounter + " total cards requested since last restart.");
+    res.render("index.ejs", {trope: squares});
+})
+
+app.get("/api/trope", (req,res) => {
+    let rando = Math.floor(Math.random() * tropes.length);
+    let trope = tropes[rando];
+    res.send(trope);
+})
+
+app.get("/api/tropes", (req,res) => {
+    res.json(tropes);
+})
+
+app.get("/api/allTropes", (req,res) => {
+    res.json(allTropes);
 })
 
 app.listen(port,() =>{
@@ -41,6 +73,19 @@ function generateCard(){
     }
     return cardTropes;
 }
+
+function generateExpandedCard(){
+    let tropePool = allTropes.slice();
+    let cardTropes = []
+    let rando = 0
+    for (let i = 0; i<24; i++){
+        rando = Math.floor(Math.random()*tropePool.length);
+        cardTropes[i] = tropePool[rando]
+        tropePool.splice(rando, 1);
+    }
+    return cardTropes;
+}
+
 //currently unused; returns too big of values, so probably will discard.
 function possibleBingoCards(){
     let possibleCards = 1
